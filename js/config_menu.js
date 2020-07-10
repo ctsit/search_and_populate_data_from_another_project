ccd46f7cf215b36773fac95199d1f0507892a2cf
Kyle Chesney <kyle.2493@gmail.com>
2020-07-10 14:48:14 -0400

    resurrect codebooks in new location, use placeholder for sample mapping

--- js/config_menu.js
+++ js/config_menu.js
@@ -1,10 +1,19 @@
 const json_lint_button = $("<input type='button' onclick='prettyPrint(this)' value='Validate JSON'></input>");
+const sample_map = JSON.stringify(
+        {
+            "source_field_1" : "target_field_1",
+            "source_field_2" : "target_field_2"
+        },
+        undefined,
+        2
+        );
 $(document).ready(function() {
-    const source_codebook = $(`<a target='_blank' href='${app_path_webroot_full}${app_path_webroot.slice(1)}Design/data_dictionary_codebook.php?pid=${STPipe.sourceProjectId}'><button>Source codebook</button></a>`);
-    const target_codebook = $(`<a target='_blank' href='${app_path_webroot_full}${app_path_webroot.slice(1)}Design/data_dictionary_codebook.php?pid=${STPipe.thisProjectId}'><button>Target codebook</button></a>`);
+    const source_codebook = `<a target='_blank' href='${app_path_webroot_full}${app_path_webroot.slice(1)}Design/data_dictionary_codebook.php?pid=${STPipe.sourceProjectId}'><button>Source codebook</button></a>`;
+    const target_codebook = `<a target='_blank' href='${app_path_webroot_full}${app_path_webroot.slice(1)}Design/data_dictionary_codebook.php?pid=${STPipe.thisProjectId}'><button>Target codebook</button></a>`;
     var $modal = $('#external-modules-configure-modal');
 
     $modal.on('show.bs.modal', function() {
+        console.log('modal happened');
         if ( $(this).data('module') != STPipe.modulePrefix ) {
             return;
         }
@@ -18,12 +27,18 @@ $(document).ready(function() {
             if ( $modal.data('module') != STPipe.modulePrefix ) {
                 return;
             }
+            // Force the descriptive field to show codebook buttons
+            $("tr[field='codebook_shortcuts']")
+                .children(":first")
+                .removeAttr('colspan')
+                .next() // hijack existing empty td for buttons
+                .html(source_codebook + target_codebook)
+                .attr('colspan', 2);
 
             let $tfield = $("textarea[name*='mapping___']");
-            fillIfBlank($tfield);
+            $tfield.attr('placeholder', sample_map);
             $tfield.after(json_lint_button);
-            $tfield.before(source_codebook);
-            $tfield.before(target_codebook);
+            // FIXME: buttons remain after eliminating a repeatable subsetting
             // TODO: detect click of $(button.external-modules-remove-instance) and remove convenience buttons for relevant Enabled Form
         }
 
@@ -44,14 +59,3 @@ function prettyPrint(element) {
         }
     }
 }
-
-function fillIfBlank($field) {
-    if ( !$field.val() ) {
-        default_map = {
-            "source_field_1" : "target_field_1",
-            "source_field_2" : "target_field_2"
-        };
-        $field.val(JSON.stringify(default_map, undefined, 2));
-    }
-}
-
