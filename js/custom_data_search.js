11c3f0019b56d754181c59b49714602db102147f
Kyle Chesney <kyle.2493@gmail.com>
2020-08-21 10:08:02 -0400

    support all radio layouts, map using coded values instead of choice labels

--- js/custom_data_search.js
+++ js/custom_data_search.js
@@ -100,9 +100,10 @@ function pasteValues(values) {
     for (let [key, value] of Object.entries(values)) {
         let $target_field = $(`input[name='${key}']`);
         if ($target_field.attr('class') == 'hiddenradio') {
-            // select radio assuming label exactly matches contents of source field
-            let $inputs = $target_field.siblings('.choicevert');
-            $inputs.find(`label:contains('${value}')`).click();
+            // collect all radio fields in all layouts
+            let $inputs = $target_field.siblings('[class*="choice"]');
+            // select radio assuming target coded value matches source coded value
+            $inputs.find(`[value='${value}']`).click();
         } else {
             // FIXME: does not honor desired date formatting
             $target_field.val(`${value}`);
