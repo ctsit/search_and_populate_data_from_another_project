808b378bff6ea2861d3706e482831f96c92c3759
mbentz <mbentz@taebertot2.local>
2021-01-08 12:50:02 -0500

    Dynamically updates source codebook href

--- js/config_menu.js
+++ js/config_menu.js
@@ -11,6 +11,30 @@ $(document).ready(function() {
     const source_codebook = `<a target='_blank' href='${app_path_webroot_full}${app_path_webroot.slice(1)}Design/data_dictionary_codebook.php?pid=${STPipe.sourceProjectId}'><button>Source codebook</button></a>`;
     const target_codebook = `<a target='_blank' href='${app_path_webroot_full}${app_path_webroot.slice(1)}Design/data_dictionary_codebook.php?pid=${STPipe.thisProjectId}'><button>Target codebook</button></a>`;
     var $modal = $('#external-modules-configure-modal');
+    var lastHoveredProjectID;
+
+    /* Dynamically updating the href of the source codebook is accomplished by 
+     * capturing the last hovered project in the select dropdown and updating
+     * the href when a new project is selected.
+    
+     * NOTE: Dynamically updating the href cannot be accomplished without the mouseenter event listener.
+     * This is because the change event on the select list completes before updating, resulting
+     * in the previous option being available rather than the new option that was selected. By keeping
+     * track of the last hovered Project in the dropdown, the href can be properly updated.
+    **/
+    var setSourceCodebook = function() {
+        // Attach mouseenter event listener to project list options to capture last hovered project
+        $('body').on('mouseenter', 'li.select2-results__option', function(event) {
+            var projectText = $(this).text();
+            lastHoveredProjectID = $('tr[field="target_pid"]').find('option:contains("'+projectText+'")').attr('value');
+        });
+
+        // Attach change event listener to the project select list to update href
+        $('tr[field="target_pid"]').find('select').on('change', function(event) {
+            var updatedHref = `${app_path_webroot_full}${app_path_webroot.slice(1)}Design/data_dictionary_codebook.php?pid=${lastHoveredProjectID}`;
+            $("tr[field='codebook_shortcuts'] a").first().attr('href', updatedHref);
+        });        
+    }
 
     $modal.on('show.bs.modal', function() {
         if ( $(this).data('module') != STPipe.modulePrefix ) {
@@ -40,6 +64,9 @@ $(document).ready(function() {
                 .siblings().remove(); // prevent duplicate lint buttons for a mapping field
             $mappingFields.after(json_lint_button.clone());
             // TODO: force validation of all JSON fields _before_ save
+
+            // Dynamically update source codebook
+            setSourceCodebook();
         }
 
     });
